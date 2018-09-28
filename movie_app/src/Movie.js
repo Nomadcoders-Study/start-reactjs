9f6e2e9b0c84e0b7623b5b4fb7fa8e3abb35799c
yykdev <assavictory@gmail.com>
2018-09-28 18:59:57 +0900

    PropTypes 셋팅

--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -21,6 +21,10 @@ class Movie extends Component{
 }
 
 class MoviePoster extends Component {
+
+    static propTypes = {
+        poster: PropTypes.string.isRequired
+    }
     render(){
         console.log(this.props.poster)
         return (
