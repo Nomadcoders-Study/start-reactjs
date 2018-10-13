e8b4de16d7c2d2c140eda00cdbc770176b34d2ba
yykdev <assavictory@gmail.com>
2018-10-14 00:01:34 +0900

    div 영역 별 클래스 구분

--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -5,16 +5,20 @@ import './Movie.css';
 
 function Movie({title, poster}) {
     return(
-        <div>
-            <MoviePoster poster={ poster }/>
-            <h1>{ title }</h1>
+        <div className="Movie">
+            <div className="Movie__Columns">
+                <MoviePoster poster={ poster }/>
+            </div>
+            <div className="Movie__Columns">
+                <h1>{ title }</h1>
+            </div>
         </div>
     )
 }
 
 function MoviePoster({poster}) {
     return (
-        <img src={ poster }/>
+        <img src={poster}/>
     )
 }
 
